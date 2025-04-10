d116f86efee13858bde0f857777f09f009e60e76
p-buddy <parkermalachowsky@gmail.com>
2025-04-10 22:26:14 +0000

    going back to non node converter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { MessageReaderOptions, MessageWriterOptions, ReadableStreamMessageReader, WriteableStreamMessageWriter, } from "vscode-jsonrpc";
 import { StreamMessageReader as _StreamMessageReader, StreamMessageWriter as _StreamMessageWriter } from "../node_modules/vscode-jsonrpc/lib/node/main";
+import { ReadableWebToNodeStream } from 'readable-web-to-node-stream';
 import type { WebContainerProcess } from "@webcontainer/api";
-import { Writable, Readable } from 'node:stream';
-import type { ReadableStream } from 'node:stream/web';
+import { Writable } from 'stream';
 
 export declare class StreamMessageWriterType extends WriteableStreamMessageWriter {
   constructor(writable: NodeJS.WritableStream, options?: ("ascii" | "utf-8") | MessageWriterOptions);
@@ -10,7 +10,7 @@ export declare class StreamMessageWriterType extends WriteableStreamMessageWrite
 
 export class StreamMessageReader extends _StreamMessageReader implements ReadableStreamMessageReader {
   constructor(readable: WebContainerProcess["output"], encoding?: ("ascii" | "utf-8") | MessageReaderOptions) {
-    super(Readable.fromWeb(readable as ReadableStream<any>), encoding);
+    super(new ReadableWebToNodeStream(readable) as unknown as NodeJS.ReadableStream, encoding);
   }
 }
 
