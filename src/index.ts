2db4761ed437cf3de1cf63afcad3381bf0774d53
p-buddy <parkermalachowsky@gmail.com>
2025-04-10 22:22:55 +0000

    attempting to convert streams

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import type { MessageReaderOptions, MessageWriterOptions, ReadableStreamMessageReader, WriteableStreamMessageWriter, } from "vscode-jsonrpc";
 import { StreamMessageReader as _StreamMessageReader, StreamMessageWriter as _StreamMessageWriter } from "../node_modules/vscode-jsonrpc/lib/node/main";
+import type { WebContainerProcess } from "@webcontainer/api";
+import { Writable, Readable } from 'node:stream';
+import type { ReadableStream } from 'node:stream/web';
 
-export declare class StreamMessageReaderType extends ReadableStreamMessageReader {
-  constructor(readable: NodeJS.ReadableStream, encoding?: ("ascii" | "utf-8") | MessageReaderOptions);
-}
 export declare class StreamMessageWriterType extends WriteableStreamMessageWriter {
   constructor(writable: NodeJS.WritableStream, options?: ("ascii" | "utf-8") | MessageWriterOptions);
 }
 
-const StreamMessageReader = _StreamMessageReader as any as StreamMessageReaderType;
-const StreamMessageWriter = _StreamMessageWriter as any as StreamMessageWriterType;
+export class StreamMessageReader extends _StreamMessageReader implements ReadableStreamMessageReader {
+  constructor(readable: WebContainerProcess["output"], encoding?: ("ascii" | "utf-8") | MessageReaderOptions) {
+    super(Readable.fromWeb(readable as ReadableStream<any>), encoding);
+  }
+}
 
-export { StreamMessageReader, StreamMessageWriter };
+export class StreamMessageWriter extends _StreamMessageWriter implements WriteableStreamMessageWriter {
+  constructor(writable: WebContainerProcess["input"], options?: ("ascii" | "utf-8") | MessageWriterOptions) {
+    super(Writable.fromWeb(writable), options);
+  }
+}
